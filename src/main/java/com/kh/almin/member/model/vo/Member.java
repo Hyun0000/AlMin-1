f4d6446b17cf8a542f8a50f2aaae504103371560
Seonhea <chsh9410@gmail.com>
2021-11-11 21:43:31 +0900

    첫 commit

---
+++ src/main/java/com/kh/almin/member/model/vo/Member.java
@@ -0,0 +1,18 @@
+package com.kh.almin.member.model.vo;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Data
+public class Member {
+	
+	private String memberName;
+
+	
+
+}
