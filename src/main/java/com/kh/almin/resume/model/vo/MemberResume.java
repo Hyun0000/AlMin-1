ab434c9ed30f43fdc2d09b79a101046fe93c09d0
GoSungHwa <hya980212@gmail.com>
2021-11-25 11:40:06 +0900

    21.11.25(고성화) resume vo 수정&mybatis-config resume 추가

--- src/main/java/com/kh/almin/resume/model/vo/MemberResume.java
+++ src/main/java/com/kh/almin/resume/model/vo/MemberResume.java
@@ -6,7 +6,15 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
+//RESUME_NO	NUMBER
+//MEMBER_ID	VARCHAR2(50 BYTE)
+//MEMBER_TYPE	CHAR(1 BYTE)
+//RESUME_TITLE	VARCHAR2(200 BYTE)
+//RESUME_PUBLIC_YN	CHAR(1 BYTE)
+//RESUME_PHONE	CHAR(13 BYTE)
+//JOB_DISTRICT_NO	NUMBER
+//EDUCATION_NO	NUMBER
+//CAREERS_NO	NUMBER
 @Getter
 @Setter
 @ToString
@@ -15,9 +23,11 @@ import lombok.ToString;
 public class MemberResume { //회원 이력서 테이블
 	private int resumeNo;
 	private String memberId;
-	private String memberType;
-	private int resumePhotoNo;
-	private int educationNo;
+	private char memberType;
 	private String resumeTitle;
-	private String resumeContent;
+	private char resumePublicYn;
+	private char resumePhone;
+	private int jobDistrictNo;
+	private int educationNo;
+	private int careersNo;
 }
