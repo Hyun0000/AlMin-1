75750ab1da429d7c969d045275407b9fc4772fae
PearlKim <thankyou821@naver.com>
2021-12-02 19:39:41 +0900

    ReportRecruit, mybatis-config.xml 추가

---
+++ src/main/java/com/kh/almin/recruit/model/vo/ReportRecruit.java
@@ -0,0 +1,22 @@
+package com.kh.almin.recruit.model.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@Data
+@Component
+public class ReportRecruit {
+	private int reportNo;
+	private String memberId;
+	private String memberType;
+	private int recruitNo;
+	private int reasonNo;
+
+}
